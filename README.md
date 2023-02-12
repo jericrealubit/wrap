87ac9a494e913baf1c9d08c20fe8bf14f876d9f4
jeric realubit <jericrealubit@gmail.com>
2023-02-12 22:21:25 +1300

    fist commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # wrap
-This function will accept 2 arguments: $string and $length. Will return a string with "\n" added to ensure that no line is longer than $length
+
+- This function will accept 2 arguments: $string and $length.
+- Will return a string with "\n" added to ensure that no line is longer than $length
+- implement the function directly, rather than using any built-in function (for example the wordwrap() function in PHP
+
+# Acceptance
+
+- function should return the string, but with new lines ("\n") added to ensure that no line is longer than $length characters
+- Always wrap at word boundaries if possible, only break a word if it is longer than $length characters
+
+# Output
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+// var_dump($_REQUEST);
+
+
+$str = $_REQUEST['string'];
+$len = $_REQUEST['length'];
+
+$res = wrap($str, $len);
+
+echo "<pre>";
+print_r($res);
+
+function wrap($string, $length) {
+
+  $error = false;
+
+  // input required check
+  if (empty($string) || empty($length)) {
+    echo "<br>NOTICE: 2 arguments required, string and length";
+    $error = true;
+  }
+
+  // input type check
+  if (!is_string($string)){
+    echo "<br>NOTICE: First argument must be of type 'string'";
+    $error = true;
+  }
+
+  if (!is_string($string)){
+    echo "<br>NOTICE: First argument must be of type 'string'";
+    $error = true;
+  }
+
+  if (!is_numeric($length)) {
+    echo "<br>NOTICE: Second argument must be of type 'numeric'";
+    $error = true;
+  }
+
+  if ($error) die;
+
+  // logic
+  echo "<br>string: ".$string;
+  echo "<br>length: ".$length;
+  echo "<hr>";
+
+  $cnt = 0;
+  $result = '';
+  foreach (str_split($string) as $chr) {
+    $result .= $chr;
+    $cnt++;
+    if ($cnt == $length) {
+        $result .= "<br>"; // \n
+        $cnt = 0;
+    }
+  }
+
+  return $result;
+
+}
